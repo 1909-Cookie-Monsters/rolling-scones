8ecd40e1aa8e9784fbe4522d6dfb0a6730580265
Alvin Togonon <artogalt@gmail.com>
2019-11-06 18:00:47 -0500

    fixed some component routes

--- client/routes.js
+++ client/routes.js
@@ -21,11 +21,12 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/home" component={UserHome} />
-        <Route path="/products/:id" component={SingleProduct} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/home" component={UserHome} />
+        <Route exact path="/products/:id" component={SingleProduct} />
         <Route exact path="/products" component={AllProducts} />
+        <Route exact path="/" component={UserHome} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
