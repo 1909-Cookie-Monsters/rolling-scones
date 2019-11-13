03556c88b5a919c1a883ed15fa3fc16ab8e31284
Samantha Lastig <slastig13@gmail.com>
2019-11-12 19:03:00 -0500

    hopefully last time

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {getCartThunkCreator} from '../store/cart'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {
@@ -14,34 +14,54 @@ import {
   Segment
 } from 'semantic-ui-react'
 
-const FixedMenuLayout = () => (
-  <div>
-    <Container text style={{marginTop: '7em'}}>
-      <Header as="h1">Welcome to The Rolling Scones Bakery</Header>
-      <Image
-        src="https://cdn.dribbble.com/users/2362961/screenshots/5526231/44therollingscones_1x.png"
-        style={{marginTop: '2em'}}
-      />
-      <p>
-        Established in 2019, The Rolling Scones is the hippest bakery in NYC!
-      </p>
-    </Container>
-  </div>
-)
+class FixedMenuLayout extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-export default FixedMenuLayout
+  componentDidMount() {
+    if (this.props.user.id) {
+      this.props.getOrderId()
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Container text style={{marginTop: '7em'}}>
+          <Header as="h1">Welcome to The Rolling Scones Bakery</Header>
+          <Image
+            src="https://cdn.dribbble.com/users/2362961/screenshots/5526231/44therollingscones_1x.png"
+            style={{marginTop: '2em'}}
+          />
+          <p>
+            Established in 2019, The Rolling Scones is the hippest bakery in
+            NYC!
+          </p>
+        </Container>
+      </div>
+    )
+  }
+}
+
+// export default FixedMenuLayout
 
 // /**
 //  * CONTAINER
 //  */
-// const mapState = state => {
-//   return {
-//     email: state.user.email,
-//     firstName: state.user.firstName
-//   }
-// }
+const mapState = state => {
+  return {
+    user: state.user
+  }
+}
 
-// export default connect(mapState)(UserHome)
+const mapDispatch = dispatch => {
+  return {
+    getOrderId: () => dispatch(getCartThunkCreator())
+  }
+}
+
+export default connect(mapState, mapDispatch)(FixedMenuLayout)
 
 // /**
 //  * PROP TYPES
